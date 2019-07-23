0ce100ee8a73968dd5fd6d2e8eed827e04c30e1f
sametz <>
2019-07-23 13:52:46 -0400

    Fixed filename in panelserverextension.py.

--- panelserverextension.py
+++ panelserverextension.py
@@ -1,5 +1,5 @@
 from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
-    """serve the glaciers.ipynb directory with bokeh server"""
-    Popen(["panel", "serve", "glaciers.ipynb", "--allow-websocket-origin=*"])
+    """serve the dnmr_ab.ipynb directory with bokeh server"""
+    Popen(["panel", "serve", "dnmr_ab.ipynb", "--allow-websocket-origin=*"])
