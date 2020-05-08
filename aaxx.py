b9ef128ab44dfccbd584a7d74cf0dcbe54f610b9
sametz <sametz@udel.edu>
2020-05-07 21:00:54 -0400

    Added text for AA'XX'. aaxx_app kwargs don't seem to override aaxx functions default kwargs (TODO fix this).

--- aaxx.py
+++ aaxx.py
@@ -23,24 +23,53 @@ def aaxx(va=110.0, vx=90.0,
     return hv.Curve(zip(x, y))
 
 
+# TODO: arguments below do not seem to override aaxx defaults?! Fix this.
 aaxx_app = pn.interact(aaxx,
-                       va=(0.0, 300.0, 0.1, 110.0),
-                       vx=(0.0, 300.0, 0.1, 90.0),
+                       va=(0.0, 300.0, 0.1, 140.0),
+                       vx=(0.0, 300.0, 0.1, 160.0),
                        Jaa=(0.0, 20.0, 0.1, 5.0),
                        Jxx=(0.0, 20.0, 0.1, 10.0),
                        Jax=(0.0, 20.0, 0.1, 13.0),
                        Jax_prime=(0.0, 20.0, 0.1, 13.0))
 
 aaxx_text_1 = pn.pane.Markdown('''
-#### AA'XX' text goes here
-''')
-aaxx_text_2 = pn.pane.Markdown('''text 2''')
+This is a very common second-order pattern, but it can often look like a simpler first-order pattern and be analyzed as such.  
 
+In *para*-disubstituted benzenes, at low resolution, the pattern can look like a pair of doublets. Higher resolution and closer inspection reveals extra peaks around the base of each doublet. However, it is often interpreted as a doublet whose splitting is *J<sub>AX</sub>*, and this is close enough for structural analysis.
+
+The interactive plot shown here is for another common case, for two coupled methylene groups X-CH<sub>2</sub>-CH<sub>2</sub>-Y. By the "n + 1" rule, this should give rise to a pair of triplets. However, in acyclic systems, this is often not the case.
+''')
+aaxx_text_2 = pn.pane.Markdown('''
+- If the two methylene signals are close together in chemical shift,
+  significant distortions appear. 
+  The plot should initialize as a "messtet" of two signals at 140 Hz and 160 Hz. 
+  Slide &nu;<sub>a</sub> lower and/or &nu;<sub>x</sub> higher. 
+  As separation increases, the signals start to resemble a pair of triplets. 
+  Keep going until &nu;<sub>a</sub> is 15 Hz and &nu;<sub>x</sub> is 200 Hz.
+''')
+aaxx_text_3 = pn.pane.Markdown('''
+When there is no strong conformational preference for *anti-* over *gauche-* conformers 
+(rotating about the C-C bond), 
+you will often see this first-order behavior of two triplets. 
+The *J*<sub>AX</sub> and *J*<sub>AX'</sub> values of 5.5 
+are a weighted average of the coupling constants seen in the rapidly-interconverting *anti-* and *gauche-* conformers 
+of equal population (33 1/3 % of each). 
+''')
+aaxx_text_4 = pn.pane.Markdown('''
+- Continuing with the interactive plot above, 
+  change Jax to 3.2 Hz and Jax_prime to 10.1 Hz. 
+  This simulates a system that has an 80% preference for the anti-conformation. 
+  The signal has changed appearance, 
+  with the outermost peaks being larger than the inner peaks. 
+  This is a very common situation that suggests a second-order effect is in play--
+  a normal "*n*+1" multiplet has the smallest peaks on the outside, 
+  and gets larger towards the middle. 
+''')
 backward = pn.widgets.Button(name='\u25c0', width=50)
 forward = pn.widgets.Button(name='\u25b6', width=50)
 back_fwd = pn.Row(backward, forward)
 
-test_text = [aaxx_text_1, aaxx_text_2]
+test_text = [aaxx_text_1, aaxx_text_2, aaxx_text_3, aaxx_text_4]
 deque_text = deque(test_text)
 text_column = pn.Column(back_fwd, deque_text[0])
 
