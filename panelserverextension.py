cacae4a31c0d90b5c2537a40214693f7bef3b617
sametz <sametz@udel.edu>
2020-06-19 20:28:45 -0400

    edit panelserverextension, trying to get mybinder to work

--- panelserverextension.py
+++ panelserverextension.py
@@ -2,4 +2,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the dnmr_ab.ipynb directory with bokeh server"""
-    Popen(["panel", "serve", "dnmr_ab.ipynb", "--allow-websocket-origin=*"])
+    Popen(["panel", "serve", "chem333test.ipynb", "--allow-websocket-origin=*"])
